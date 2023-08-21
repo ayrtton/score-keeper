78d697435e97ed44fe0bee64478f004fa26867ff
Ayrton <ayrtton.vinicius@gmail.com>
2023-08-21 09:54:41 -0300

    Create functions for score increase and reset

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
 let maximumScore = 10;
-let player01Score = 0;
-let player02Score = 0;
+const playerScores = [0, 0];
+const score = document.getElementById("score");
+
+function increaseScore(player) {
+    playerScores[player]++;
+    score.innerText = `${playerScores[0]} to ${playerScores[1]}`;
+
+    if(playerScores[player] >= maximumScore) {
+        window.alert(`Player ${player + 1} is the winner!`);
+        reset();
+    }
+}
+
+function reset() {
+    playerScores[0] = playerScores[1] = 0;
+    score.innerText = "0 to 0";
+}
 
 function main() {
     const player01Button = document.getElementById("player01Button");
     const resetButton = document.getElementById("resetButton");
 
-    player01Button.addEventListener("click", () => { player01Score++; });
-    player02Button.addEventListener("click", () => { player02Score++; });
-    resetButton.addEventListener("click", () => { player01Score = player02Score = 0; });
+    player01Button.addEventListener("click", () => { increaseScore(0); });
+    player02Button.addEventListener("click", () => { increaseScore(1); });
+    resetButton.addEventListener("click", () => { reset() });
 }
 
 main();
